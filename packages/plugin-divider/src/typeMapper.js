edb61a9ff95b5eca5775f5fcef5d2d0c7a3aef5b
lxgreen <alxgreen@gmail.com>
2018-08-01 18:49:17 +0300

    [divider] feat(flow): PluginMapping is used for divider mapper

--- packages/plugin-divider/src/typeMapper.js
+++ packages/plugin-divider/src/typeMapper.js
@@ -1,6 +1,9 @@
+/* global PluginMapping */
+
 import DividerComponent from './components/divider-component';
 import { DIVIDER_TYPE } from './constants';
 
+const mapper : PluginMapping = { component: DividerComponent };
 export const typeMapper = () => ({
-  [DIVIDER_TYPE]: { component: DividerComponent }
+  [DIVIDER_TYPE]: mapper
 });
